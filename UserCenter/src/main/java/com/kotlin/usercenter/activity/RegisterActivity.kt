e385ff2f3f8e5a3cb22073169bc0799b39d75ce6
田叶军 <yejuntian@pptv.com>
2019-12-11 14:12:12 +0800

    [Feature]:添加用户中心可直接运行模块

---
+++ UserCenter/src/main/java/com/kotlin/usercenter/activity/RegisterActivity.kt
@@ -0,0 +1,21 @@
+package com.kotlin.usercenter.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.kotlin.usercenter.R
+
+/**
+ * <注册界面>
+ *
+ * @author  tianyejun
+ * @version  [版本号]
+ * @see  [参考资料]
+ * @since  [历史 创建日期:2019-12-11]
+ */
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
