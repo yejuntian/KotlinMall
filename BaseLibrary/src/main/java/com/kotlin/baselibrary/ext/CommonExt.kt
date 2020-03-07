29eb83357e71b4f2f865931e03fce394153bcad8
田叶军 <yejuntian@pptv.com>
2020-03-08 00:33:55 +0800

    [Feature]:添加底部导航组件

--- BaseLibrary/src/main/java/com/kotlin/baselibrary/ext/CommonExt.kt
+++ BaseLibrary/src/main/java/com/kotlin/baselibrary/ext/CommonExt.kt
@@ -11,11 +11,17 @@ import com.kotlin.baselibrary.rx.BaseSubscriber
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import java.lang.Exception
 
 fun <T> Observable<T>.execute(subscriber: BaseSubscriber<T>) {
-    return this.observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.io())
-            .subscribe(subscriber)
+    try {
+        return this.observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(subscriber)
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
 }
 
 /**
