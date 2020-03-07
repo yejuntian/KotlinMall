722ee5c4aa59bc7d406687690f63cdc4d9dd5e3a
田叶军 <yejuntian@pptv.com>
2020-03-08 01:05:00 +0800

    [Feature]:添加主页面资源文件

--- app/src/main/java/com/kotlin/mall/ui/MainActivity.kt
+++ app/src/main/java/com/kotlin/mall/ui/MainActivity.kt
@@ -1,13 +1,90 @@
 package com.kotlin.mall.ui
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import com.ashokvarma.bottomnavigation.BottomNavigationBar
 import com.kotlin.mall.R
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var pressTime: Long = 0
+    //Fragment 栈管理
+    private val mStack = Stack<Fragment>()
+    //主界面Fragment
+//    private val mHomeFragment by lazy { HomeFragment() }
+    //商品分类Fragment
+//    private val mCategoryFragment by lazy { CategoryFragment() }
+    //购物车Fragment
+//    private val mCartFragment by lazy { CartFragment() }
+    //消息Fragment
+//    private val mMsgFragment by lazy { MessageFragment() }
+    //"我的"Fragment
+//    private val mMeFragment by lazy { MeFragment() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFragment()
+        initBottomNav()
+        changeFragment(0)
+//        initObserve()
+//        loadCartSize()
+    }
+
+    /*
+        初始化Fragment栈管理
+     */
+    private fun initFragment() {
+        val manager = supportFragmentManager.beginTransaction()
+//        manager.add(R.id.mContaier,mHomeFragment)
+//        manager.add(R.id.mContaier,mCategoryFragment)
+//        manager.add(R.id.mContaier,mCartFragment)
+//        manager.add(R.id.mContaier,mMsgFragment)
+//        manager.add(R.id.mContaier,mMeFragment)
+        manager.commit()
+
+//        mStack.add(mHomeFragment)
+//        mStack.add(mCategoryFragment)
+//        mStack.add(mCartFragment)
+//        mStack.add(mMsgFragment)
+//        mStack.add(mMeFragment)
+
+    }
+
+    /*
+        初始化底部导航切换事件
+     */
+    private fun initBottomNav() {
+        mBottomNavBar.setTabSelectedListener(object : BottomNavigationBar.OnTabSelectedListener {
+            override fun onTabReselected(position: Int) {
+            }
+
+            override fun onTabUnselected(position: Int) {
+            }
+
+            override fun onTabSelected(position: Int) {
+                changeFragment(position)
+            }
+        })
+
+        mBottomNavBar.checkMsgBadge(false)
+    }
+
+    /*
+        切换Tab，切换对应的Fragment
+     */
+    private fun changeFragment(position: Int) {
+        if (mStack.empty()) return
+        val manager = supportFragmentManager.beginTransaction()
+        for (fragment in mStack) {
+            manager.hide(fragment)
+        }
+
+        manager.show(mStack[position])
+        manager.commit()
     }
 }
