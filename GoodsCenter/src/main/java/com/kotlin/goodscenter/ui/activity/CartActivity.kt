7e45e557b4e23570ac5c20ffc6b879ffae4c58c1
田叶军 <yejuntian@pptv.com>
2020-03-08 12:57:06 +0800

    [Feature]:完成ARouter路由跳转

--- GoodsCenter/src/main/java/com/kotlin/goodscenter/ui/activity/CartActivity.kt
+++ GoodsCenter/src/main/java/com/kotlin/goodscenter/ui/activity/CartActivity.kt
@@ -1,6 +1,7 @@
 package com.kotlin.goods.ui.activity
 
 import android.os.Bundle
+import com.alibaba.android.arouter.facade.annotation.Route
 import com.kotlin.baselibrary.ui.activity.BaseActivity
 import com.kotlin.goodscenter.R
 
@@ -8,13 +9,11 @@ import com.kotlin.goodscenter.R
     购物车Activity
     只是Fragment一个壳
  */
+@Route(path = "/goods/cart_activity")
 class CartActivity: BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cart)
-
-        val fragment = supportFragmentManager.findFragmentById(R.id.fragment_cart)
-//        (fragment as CartFragment).setBackVisible(true)
+        setContentView(R.layout.activity_goods)
 
     }
 }
--- app/src/main/java/com/kotlin/mall/fragment/MeFragment.kt
+++ app/src/main/java/com/kotlin/mall/fragment/MeFragment.kt
@@ -5,7 +5,10 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.alibaba.android.arouter.launcher.ARouter
+import com.kotlin.baselibrary.ext.onClick
 import com.kotlin.mall.R
+import kotlinx.android.synthetic.main.fragment_me_layout.*
 
 /**
  * <类说明 必填>
@@ -18,6 +21,22 @@ import com.kotlin.mall.R
 </类说明> */
 class MeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_me_layout,container,false)
+        return inflater.inflate(R.layout.fragment_me_layout, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initListener();
+    }
+
+    private fun initListener() {
+        btn_jump.onClick(View.OnClickListener {
+            ARouter.getInstance().build("/goods/cart_activity")
+//                    .withString("name", "MeFragment")
+//                    .withInt("age", 1)
+                    .navigation()
+        })
+    }
+
+
 }
